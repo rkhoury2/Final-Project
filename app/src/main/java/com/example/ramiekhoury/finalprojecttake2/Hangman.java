c9ce6d38ff22b8a15e0c894a01db4fb2e0ae5899
Cassandra Afseth <cafseth2@Illinois.edu>
2018-04-24 01:33:24 -0500

    attached button to main activity?

--- app/src/main/java/com/example/ramiekhoury/finalprojecttake2/Hangman.java
+++ app/src/main/java/com/example/ramiekhoury/finalprojecttake2/Hangman.java
@@ -5,9 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class Hangman extends AppCompatActivity {
 
@@ -15,18 +17,15 @@ public class Hangman extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hangman);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        final Button restartButton = findViewById(R.id.button_id);
+        restartButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Log.d("restart button", "The restart button works!");
             }
         });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
